6ff5066234dac4542b03ec26a74e9f12dd944ed0
monsieur-pivert <84725283+monsieur-pivert@users.noreply.github.com>
2021-07-12 22:12:31 +0200

    Ajouter des intervalles

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ DuréeNote = 45
 NombreNotesDictée = 2
 IntervalleMaxi = 8
 nombreRépétitionsNotes = 13
+IntervalleMini = 1
 
 NotesPossibles = [
     Note.C4,
@@ -51,7 +52,29 @@ NomsNotesPossibles = [
         "Sol#5",
     ]
 
+Intervalles = [
+    "",
+"Seconde mineure",
+    "Seconde majeure",
+    "Tierce mineure",
+    "Tierce majeure",
+    "Quarte juste",
+#    "Quarte diminuee",
+#    "Quarte augmentee",
+"Quinte diminuee",
+    "Quinte juste",
+    "Quinte augmentee",
+    "Sixte majeure",
+#    "Sixte mineure",
+ "Septième mineure",
+ "Septième majeure",
+     "Octave juste",
+    ]
+
 NotesDictée : List[number] = notesAuHasard(NotesPossibles)
+
+def calculerIntervalle(indexPremièreNote, indexDeuxièmeNote):
+    return Math.abs(indexDeuxièmeNote - indexPremièreNote)
 
 def notesAuHasard(listeNotes):
         notes : List[number] = []
@@ -59,8 +82,8 @@ def notesAuHasard(listeNotes):
         for i in range(NombreNotesDictée):
             while True:
                 indexNote : number = randint(0, NotesPossibles.length - 1)
-                intervalle = Math.abs(indexNote - indexNotePrécédente)
-                if intervalle <= IntervalleMaxi:
+                intervalle = calculerIntervalle(indexNote, indexNotePrécédente)
+                if intervalle <= IntervalleMaxi and intervalle >= IntervalleMini:
                     break
             notes.append(indexNote)
             indexNotePrécédente = indexNote
@@ -69,6 +92,14 @@ def notesAuHasard(listeNotes):
 def afficherDictéeNote() -> void :
         for note in NotesDictée:
             basic.show_string(NomsNotesPossibles[note])
+
+def afficherIntervalle() -> void :
+    indexPremièreNote : number = NotesDictée[0]
+    indexDeuxièmeNote : number = NotesDictée[1]
+    indexIntervalle = calculerIntervalle(indexPremièreNote, indexDeuxièmeNote)
+    i = Intervalles[indexIntervalle]
+    basic.show_string(i)
+
 
 def afficherPremièreNote():
         indexPremièreNote : number = NotesDictée[0]
@@ -87,4 +118,5 @@ def jouerDictéeNote() -> void :
         music.play_tone(deuxièmeNoteAccord, DuréeNote)
 
 input.on_button_pressed(Button.A, jouerDictéeNote)
-input.on_button_pressed(Button.B, afficherDictéeNote)
+input.on_button_pressed(Button.AB, afficherDictéeNote)
+input.on_button_pressed(Button.B, afficherIntervalle)
--- main.ts
+++ main.ts
@@ -2,9 +2,18 @@ let DuréeNote = 45
 let NombreNotesDictée = 2
 let IntervalleMaxi = 8
 let nombreRépétitionsNotes = 13
+let IntervalleMini = 1
 let NotesPossibles = [Note.C4, Note.CSharp4, Note.D4, Note.Eb4, Note.E4, Note.F4, Note.FSharp4, Note.G4, Note.GSharp4, Note.A4, Note.Bb4, Note.B4, Note.C5, Note.CSharp5, Note.D5, Note.Eb5, Note.E5, Note.F5, Note.FSharp5, Note.G5, Note.GSharp5]
 let NomsNotesPossibles = ["Do", "Do#", "Re", "MiB", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "SiB", "Si", "Do5", "Do#5", "Re5", "MiB5", "Mi5", "Fa5", "Fa#5", "Sol5", "Sol#5"]
+let Intervalles = ["", "Seconde mineure", "Seconde majeure", "Tierce mineure", "Tierce majeure", "Quarte juste", "Quinte diminuee", "Quinte juste", "Quinte augmentee", "Sixte majeure", "Septième mineure", "Septième majeure", "Octave juste"]
+//     "Quarte diminuee",
+//     "Quarte augmentee",
+//     "Sixte mineure",
 let NotesDictée = notesAuHasard(NotesPossibles)
+function calculerIntervalle(indexPremièreNote: number, indexDeuxièmeNote: number): number {
+    return Math.abs(indexDeuxièmeNote - indexPremièreNote)
+}
+
 function notesAuHasard(listeNotes: any): number[] {
     let indexNote: number;
     let intervalle: number;
@@ -13,8 +22,8 @@ function notesAuHasard(listeNotes: any): number[] {
     for (let i = 0; i < NombreNotesDictée; i++) {
         while (true) {
             indexNote = randint(0, NotesPossibles.length - 1)
-            intervalle = Math.abs(indexNote - indexNotePrécédente)
-            if (intervalle <= IntervalleMaxi) {
+            intervalle = calculerIntervalle(indexNote, indexNotePrécédente)
+            if (intervalle <= IntervalleMaxi && intervalle >= IntervalleMini) {
                 break
             }
             
@@ -42,8 +51,15 @@ input.onButtonPressed(Button.A, function jouerDictéeNote() {
         music.playTone(deuxièmeNoteAccord, DuréeNote)
     }
 })
-input.onButtonPressed(Button.B, function afficherDictéeNote() {
+input.onButtonPressed(Button.AB, function afficherDictéeNote() {
     for (let note of NotesDictée) {
         basic.showString(NomsNotesPossibles[note])
     }
+})
+input.onButtonPressed(Button.B, function afficherIntervalle() {
+    let indexPremièreNote = NotesDictée[0]
+    let indexDeuxièmeNote = NotesDictée[1]
+    let indexIntervalle = calculerIntervalle(indexPremièreNote, indexDeuxièmeNote)
+    let i = Intervalles[indexIntervalle]
+    basic.showString(i)
 })
